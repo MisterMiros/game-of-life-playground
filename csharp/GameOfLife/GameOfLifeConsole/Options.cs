7fc25659719e35ea8918f9bcaccfa80312a88c37
Miroslav Bogatyrev <mistermiros@gmail.com>
2025-09-08 22:34:11 +0200

    Add command-line argument parsing to `ConsoleRunner` with grid size and initial cell configuration file support

---
+++ csharp/GameOfLife/GameOfLifeConsole/Options.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+
+namespace GameOfLifeConsole;
+
+public class Options
+{
+    [Option(
+        'f',
+        "file",
+        Required = false,
+        HelpText =
+            "File to read the initial set of active cells from. If omitted, cells will be read from standard input.")]
+    public string? File { get; init; }
+
+    [Option('s', "size", Required = false, HelpText = "Size of the square grid", Default = 1000)]
+    public int Size { get; init; }
+}
